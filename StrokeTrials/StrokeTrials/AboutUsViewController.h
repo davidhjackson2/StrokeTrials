83637b384e80122a0a7bf894d253de48e0cbaf35
everyusernamestaken <brenden.leonard@gatech.edu>
2014-04-20 23:51:29 -0400

    constraints are fixed

---
+++ StrokeTrials/StrokeTrials/AboutUsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutUsViewController.h
+//  StrokeTrials
+//
+//  Created by Brenden Leonard on 4/20/14.
+//  Copyright (c) 2014 The Mullets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutUsViewController : UIViewController
+
+@end
