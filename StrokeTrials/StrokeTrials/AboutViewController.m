e1cc63289e777db5b2a096a9d3d716054928c695
djackson46 <djackson46@gatech.edu>
2014-06-11 21:36:39 -0400

    Updated About section

--- StrokeTrials/StrokeTrials/AboutViewController.m
+++ StrokeTrials/StrokeTrials/AboutViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "AboutViewController.h"
+
 int nextLabel = 65;
 NSString *anyUrl = @"";
 NSString *anyUrlText = @"";
@@ -33,81 +34,105 @@ NSString *anyUrlText = @"";
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     NSURL *url = [NSURL URLWithString:@"https://sites.google.com/site/stroketrialsapp/home/manager/About.xml"];
     parser = [[NSXMLParser alloc] initWithContentsOfURL:url];
     [parser setDelegate:self];
     [parser setShouldResolveExternalEntities:NO];
     [parser parse];
+    
+    nextLabel = 65;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
 
 - (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict {
     element = elementName;
     if ([element isEqualToString:@"about"]) {
         xmlDict = [[NSMutableDictionary alloc] init];
-        //xmlTrial = [[Trial alloc] init];
-    }
-}
-
-- (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName {
-    if ([elementName isEqualToString:@"about"]) {
-        //[xmlDict setObject:xmlTrial forKey:@"trial"];
-        //[self.trials addObject:[xmlDict copy]];
     }
 }
 
 - (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string {
-    if ([element isEqualToString:@"section"]) {
-        if (![string isEqualToString: @"\n"]) {
-            UILabel *headingLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, nextLabel+=5, 300, 17)];
-            nextLabel+=22;
-            [headingLabel setFont:[UIFont fontWithName:@"Arial-BoldMT" size:16.0]];
-            headingLabel.text = string;
-            headingLabel.textAlignment = NSTextAlignmentCenter;
-            [self.view addSubview:headingLabel];
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+        if ([element isEqualToString:@"section"]) {
+            if (![string isEqualToString: @"\n"]) {
+                UILabel *headingLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, nextLabel += 10, 300, 16)];
+                nextLabel += 25;
+                [headingLabel setFont:[UIFont fontWithName:@"Arial-BoldMT" size:14.0]];
+                headingLabel.text = string;
+                headingLabel.textAlignment = NSTextAlignmentCenter;
+                [self.view addSubview:headingLabel];
+            }
+        } else if ([element isEqualToString:@"info"]) {
+            if (![string isEqualToString: @"\n"]) {
+                UILabel *headingLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, nextLabel, 300, 16)];
+                nextLabel += 25;
+                headingLabel.font = [UIFont fontWithName:@"Arial" size:14.0];
+                headingLabel.text = string;
+                headingLabel.textAlignment = NSTextAlignmentCenter;
+                [self.view addSubview:headingLabel];
+            }
+        } else if ([element isEqualToString:@"logo"]) {
+            if (![string isEqualToString: @"\n"]) {
+                anyUrlText = string;
+            }
+        } else if ([element isEqualToString:@"link"]) {
+            if (![string isEqualToString: @"\n"]) {
+                anyUrl = string;
+                UIButton *logoLink = [[UIButton alloc] initWithFrame:CGRectMake(10, nextLabel, 300, 16)];
+                nextLabel += 25;
+                [logoLink setTitle:anyUrlText forState:UIControlStateNormal];
+                [logoLink setTitleColor: [UIColor blueColor] forState: UIControlStateNormal];
+                logoLink.titleLabel.font = [UIFont systemFontOfSize:14.0];
+                [logoLink addTarget:self action:@selector(buttonPressed) forControlEvents:UIControlEventTouchUpInside];
+                [self.view addSubview:logoLink];
+            }
         }
-    } else if ([element isEqualToString:@"info"]) {
-        if (![string isEqualToString: @"\n"]) {
-            UILabel *headingLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, nextLabel, 300, 16)];
-            nextLabel+=21;
-            headingLabel.font = [UIFont fontWithName:@"Arial" size:15.0];
-            headingLabel.text = string;
-            headingLabel.textAlignment = NSTextAlignmentCenter;
-            [self.view addSubview:headingLabel];
-        }
-    } else if ([element isEqualToString:@"logo"]) {
-        if (![string isEqualToString: @"\n"]) {
-            anyUrlText = string;
-        }
-    } else if ([element isEqualToString:@"link"]) {
-        if (![string isEqualToString: @"\n"]) {
-            anyUrl = string;
-            UIButton *logoLink = [[UIButton alloc] initWithFrame:CGRectMake(10, nextLabel, 300, 16)];
-            nextLabel+=21;
-            [logoLink setTitle:anyUrlText forState:UIControlStateNormal];
-            [logoLink setTitleColor: [UIColor blueColor] forState: UIControlStateNormal];
-            [logoLink addTarget:self action:@selector(buttonPressed) forControlEvents:UIControlEventTouchUpInside];
-            [self.view addSubview:logoLink];
+    }
+    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
+        if ([element isEqualToString:@"section"]) {
+            if (![string isEqualToString: @"\n"]) {
+                UILabel *headingLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, nextLabel += 10, 680, 16)];
+                nextLabel += 40;
+                [headingLabel setFont:[UIFont fontWithName:@"Arial-BoldMT" size:18.0]];
+                headingLabel.text = string;
+                headingLabel.textAlignment = NSTextAlignmentCenter;
+                [self.view addSubview:headingLabel];
+            }
+        } else if ([element isEqualToString:@"info"]) {
+            if (![string isEqualToString: @"\n"]) {
+                UILabel *headingLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, nextLabel, 680, 16)];
+                nextLabel += 40;
+                headingLabel.font = [UIFont fontWithName:@"Arial" size:18.0];
+                headingLabel.text = string;
+                headingLabel.textAlignment = NSTextAlignmentCenter;
+                [self.view addSubview:headingLabel];
+            }
+        } else if ([element isEqualToString:@"logo"]) {
+            if (![string isEqualToString: @"\n"]) {
+                anyUrlText = string;
+            }
+        } else if ([element isEqualToString:@"link"]) {
+            if (![string isEqualToString: @"\n"]) {
+                anyUrl = string;
+                UIButton *logoLink = [[UIButton alloc] initWithFrame:CGRectMake(10, nextLabel, 680, 16)];
+                nextLabel += 40;
+                [logoLink setTitle:anyUrlText forState:UIControlStateNormal];
+                [logoLink setTitleColor: [UIColor blueColor] forState: UIControlStateNormal];
+                logoLink.titleLabel.font = [UIFont systemFontOfSize:18.0];
+                [logoLink addTarget:self action:@selector(buttonPressed) forControlEvents:UIControlEventTouchUpInside];
+                [self.view addSubview:logoLink];
+            }
         }
     }
 }
 
-- (void)buttonPressed{
+- (void)buttonPressed {
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString: anyUrl]];
 }
 
